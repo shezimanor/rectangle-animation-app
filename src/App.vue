0aebab600395fec3c16c71cddb2ec3080ebb45f0
Ryan Chen <y.f.chenroderick@gmail.com>
2024-06-19 01:59:46 +0800

    feat: 建立選擇數量選項功能，並調整版面排版 --- 同時也建立單選按鈕(Radio Button)，但功能尚未實現

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,51 @@
 <script setup>
+import { computed, ref } from 'vue';
 import GeometricShape from './components/GeometricShape.vue';
+const activeElement = ref('all');
+const elementCount = ref(1);
+const elementCountSqrt = computed(() => Math.sqrt(elementCount.value));
+const elementLayoutClass = computed(
+  () => `grid-${elementCountSqrt.value}x${elementCountSqrt.value}`
+);
+function changeElementCount(count) {
+  elementCount.value = count;
+}
 </script>
 
 <template>
-  <div class="grid-container grid-5x5">
-    <GeometricShape v-for="n in 25" :active="true" />
+  <div class="main-wrapper">
+    <div :class="['grid-container', elementLayoutClass]">
+      <GeometricShape v-for="n in elementCount" :active="true" />
+    </div>
+    <div class="flex-container">
+      <div class="flex-item">
+        <button @click="changeElementCount(1)">1x1</button>
+        <button @click="changeElementCount(9)">3x3</button>
+        <button @click="changeElementCount(25)">5x5</button>
+        <button @click="changeElementCount(100)">10x10</button>
+      </div>
+      <div class="flex-item">
+        <label>
+          <input
+            type="radio"
+            id="all"
+            name="activeElement"
+            value="all"
+            v-model="activeElement"
+          />
+          All
+        </label>
+        <label>
+          <input
+            type="radio"
+            id="random"
+            name="activeItem"
+            value="random"
+            v-model="activeElement"
+          />
+          Random
+        </label>
+      </div>
+    </div>
   </div>
 </template>
