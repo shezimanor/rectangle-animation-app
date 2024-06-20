91e8d58df989604d6db17eb26f8456a146aeb7c4
Ryan Chen <y.f.chenroderick@gmail.com>
2024-06-20 11:02:34 +0800

    refactor: 重構 computed 的寫法以維護程式碼一致性 --- 將 computed 內部宣告的變數改成 ref 或 computed，以維持程式碼撰寫數據引用的一致性。

--- src/components/GeometricShape.vue
+++ src/components/GeometricShape.vue
@@ -21,6 +21,14 @@ const animateElement = ref(null);
 // 預設的 svg 的長寬
 const defaultSize = ref(360);
 
+// 圓角對應列表：borderRadius 使用
+const borderRadiusMapping = ref({
+  1: 8,
+  3: 6,
+  5: 4,
+  10: 2
+});
+
 // svg 的長寬：交由 chunk 來決定
 const size = computed(() => (defaultSize.value / chunk.value) | 0);
 
@@ -28,33 +36,31 @@ const viewBox = computed(() => `0 0 ${size.value + 4} ${size.value + 4}`);
 
 // svg 圓角寬度
 const borderRadius = computed(() => {
-  const mapping = {
-    1: 8,
-    3: 6,
-    5: 4,
-    10: 2
-  };
-  return mapping[props.chunk];
+  return borderRadiusMapping.value[chunk.value];
 });
 
-// 矩形周長
+// 矩形周長與半周長
 const shapePerimeter = computed(() => size.value * 4);
+const shapeHalfPerimeter = computed(() => size.value * 2);
+
+// 主要跑動的線條長度
+const mainDashLength = computed(() =>
+  active.value ? Math.floor(shapeHalfPerimeter.value / 3) : 0
+);
 
 // 虛線設定：兩條短虛線（周長的一半的 1:2）
 const strokeDasharray = computed(() => {
-  const shapeHalfPerimeter = size.value * 2;
-  const mainDashLength = Math.floor(shapeHalfPerimeter / 3);
-  return active.value
-    ? `${mainDashLength} ${shapeHalfPerimeter - mainDashLength}`
-    : `0 ${shapePerimeter.value}`;
+  return `${mainDashLength.value} ${
+    shapeHalfPerimeter.value - mainDashLength.value
+  }`;
 });
 
 // 監控 chunk 來控制動畫重啟：因為剛好 chunk 的變化是動畫需要重啟的主要原因
 watch(chunk, () => {
-  beginAnmation();
+  if (active.value) beginAnmation();
 });
 
-// 監控 chunk 來控制動畫重啟：
+// 監控 active 來控制動畫的啟閉
 watch(
   active,
   (newValue, oldValue) => {
@@ -69,13 +75,11 @@ watch(
 
 // methods
 function beginAnmation() {
-  console.log('beginAnmation');
   // beginElement 會立即重新啟動動畫
   animateElement.value?.beginElement();
 }
 
 function endAnmation() {
-  console.log('endAnmation');
   // endElement 會立即終止動畫
   animateElement.value?.endElement();
 }
